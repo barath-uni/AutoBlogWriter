a8fb6f2aa6a872ede8c41b9606868813493eb0bf
barathmonk <diputs.creators@gmail.com>
2022-07-23 18:18:40 +0530

    second commit

--- driver.py
+++ driver.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from read_links import *
 from get_affliate_links import *
+from keywordripper import *
 user = ""
 pwd = ""
 driver = webdriver.Chrome('chromedriver.exe')
@@ -11,7 +12,7 @@ def main():
     driver.get("https://www.amazon.in/")
     assert "Amazon" in driver.title
     # Loop through each keyword and enter it in the reader method
-    enter_keyword(driver, ["RGB lights", "rgb led"])
+    enter_keyword2(driver, ["RGB lights", "rgb led"])
     # get_affiliate_link(driver, "Sunding SD 548 B 14 Function Waterproof Bicycle Computer Odometer Speedometer")
     tearDown()
 

607516b5739d444c3fd2bd5a005a5d7c7069eb35
baradwaj varadharajan <baradwaj.varadharajan@triply.cc>
2022-12-06 15:17:15 +0100

    some minor changes to run on lisa

--- driver.py
+++ driver.py
@@ -1,29 +1,26 @@
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from pipeline.review.read_links import *
-from pipeline.review.get_affliate_links import *
+# from pipeline.review.read_links import *
+# from pipeline.review.get_affliate_links import *
 from pipeline.paa.bin import clean_and_generate_summary
-from gptgenerator.gpt_content_writer import write_an_article
+# from gptgenerator.gpt_content_writer import write_an_article
 user = ""
 pwd = ""
-from webdriver_manager.chrome import ChromeDriverManager
 
-def generate_affiliate_articles(values:list):
-    for value in values:
-        driver = webdriver.Chrome(ChromeDriverManager().install())
-        driver.get("https://www.amazon.in/")
-        assert "Amazon" in driver.title
-        # # Loop through each keyword and enter it in the reader method
-        enter_keyword(driver, [value])
-        # Get GPT Content
-        # start_time=time.time()
-        # file_path = write_an_article(title=value, variations=1)
-        # write_gpt_content_to_file(f"{value}, air cooler with lcd controls, on the floor, photo realistic, 4K HD,near a window, few plants nearby, clean atmosphere",file_path)
-        # print(f"IT TOOK IN TOTAL = {time.time()-start_time}")
-        driver.close()
+# def generate_affiliate_articles(values:list):
+#     for value in values:
+#         driver = webdriver.Chrome(ChromeDriverManager().install())
+#         driver.get("https://www.amazon.in/")
+#         assert "Amazon" in driver.title
+#         # # Loop through each keyword and enter it in the reader method
+#         enter_keyword(driver, [value])
+#         # Get GPT Content
+#         # start_time=time.time()
+#         # file_path = write_an_article(title=value, variations=1)
+#         # write_gpt_content_to_file(f"{value}, air cooler with lcd controls, on the floor, photo realistic, 4K HD,near a window, few plants nearby, clean atmosphere",file_path)
+#         # print(f"IT TOOK IN TOTAL = {time.time()-start_time}")
+#         driver.close()
 
-def tearDown(): 
-    driver.close()
+# def tearDown(): 
+#     driver.close()
 
 
 if __name__ == "__main__":
